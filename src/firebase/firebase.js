82681c201ea9271a4803321d334932499ffe8672
ordd2641 <ordd2641@gmail.com>
2026-06-13 23:12:34 -0700

    1

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from 'firebase/app';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyA4zucwY22zK2ugiUGtrln86C79vRioXaE",
+  authDomain: "continental-b7db6.firebaseapp.com",
+  projectId: "continental-b7db6",
+  storageBucket: "continental-b7db6.firebasestorage.app",
+  messagingSenderId: "607198002974",
+  appId: "1:607198002974:web:b4b6e45c237af51b44930f"
+};
+
+export const app = initializeApp(firebaseConfig);
